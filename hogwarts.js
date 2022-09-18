9f541b98e2c3e8bae0b6695d66b9a4f197003cd4
Sabine <sabi4826@stud.kea.dk>
2022-09-18 21:27:58 +0200

    popup a little better etc.

--- hogwarts.js
+++ hogwarts.js
@@ -89,9 +89,7 @@ function prepareObjects(jsonData) {
     // (fullName.includes("-"))
 
     // NICK NAME:
-    //if (fullName.indexOf("\\"))
-    //let nickname = fullName.substring(fullName.indexOf("\\") + 2, fullName.lastIndexOf("\\")); // a double backslash equals one i " "
-    //console.log("nickname is:", nickname);
+    let nickname = findNickName(fullName);
 
     // IMAGES:
     // return values from function haveImg: code from Nancy:
@@ -109,7 +107,7 @@ function prepareObjects(jsonData) {
     student.firstname = firstname;
     student.middlename = middlename;
     student.lastname = lastname;
-    //student.nickname = nickname;
+    student.nickname = nickname;
     student.image = image;
     //console.log("student.image is:", student.image); - displays same as image.
     student.house = studentHouse;
@@ -130,12 +128,29 @@ function prepareObjects(jsonData) {
   displayList(allStudents);
 }
 
-// image function, return values to function above - not properly though:
-function haveImg(fullName) {
-  // images are displayed with the last name and first letter of the first name
-  let imgSrc = `../images/${fullName.substring(fullName.lastIndexOf(" ") + 1).toLowerCase()}_${fullName.substring(0, 1).toLowerCase()}.png`;
+// FIND NICKNAME:
+function findNickName(fullName) {
+  let bloodyNickName;
+  if (fullName.includes("ernie")) {
+    bloodyNickName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" "));
+  } else {
+    bloodyNickName = "none";
+  }
+  return bloodyNickName;
 
+  //if (fullName.indexOf("\\"))
+  //let nickname = fullName.substring(fullName.indexOf("\\") + 2, fullName.lastIndexOf("\\")); // a double backslash equals one i " "
+  //console.log("nickname is:", nickname);
+}
+
+// IMAGE function, return values to function above - not properly though:
+function haveImg(fullName) {
+  // images are displayed with the last name and first letter of the first name:
+  let imgSrc = `../images/${fullName.substring(fullName.lastIndexOf(" ") + 1).toLowerCase()}_${fullName.substring(0, 1).toLowerCase()}.png`;
+  // not working - tried to trim down name of image only to lastname_first letter. Not working either!
+  //let img2 = imgSrc.substring(imgSrc.lastIndexOf("/") + 1);
   // for special cases:
+
   // if img has no lastname:
   if (fullName === "Leanne") {
     //console.log("imgSrc Leanne = `No_Image`");
@@ -155,9 +170,9 @@ function haveImg(fullName) {
     return (imgSrc = `../images/${fullName.substring(fullName.lastIndexOf(" ") + 1).toLowerCase()}_${fullName.substring(0, fullName.indexOf(" ")).toLowerCase()}.png`);
   }
 
+  // return img2;
   return imgSrc;
-  console.log("imgSrc: ", imgSrc); //- replaced imgSrc with image in student.image:
-  //student.image = imgSrc;
+  // console.log(imgSrc);
 }
 
 // ----------------- CONTROLLER -------------------
@@ -308,7 +323,7 @@ function showPopUp(student) {
   popup.querySelector(".art_firstname").textContent = student.firstname;
   popup.querySelector(".art_middlename").textContent = student.middlename;
   popup.querySelector(".art_lastname").textContent = student.lastname;
-  //popup.querySelector(".art_nickname").textContent = student.nickname;
+  popup.querySelector(".art_nickname").textContent = student.nickname;
   popup.querySelector(".art_gender").textContent = student.gender;
   popup.querySelector(".art_house").textContent = student.house;
   //popup.querySelector(".art_prefect").textContent = student.isPrefect;
